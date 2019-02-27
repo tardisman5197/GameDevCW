f6876bb82cfe9617f85af8f2753a4a81367816ff
Edward Evans <tardisman5197@gmail.com>
2019-02-27 14:14:35 +0000

    Created the Main Menu
    * Created a Main Menu Scene #6 Done
    * Added a temporary game title
    * Added a exit button #3 Done
    
    The exit button brings up a prompt to confirm the player's
    choice in exiting the game
    
    * Added the placeholders for Load, Controls and Start

---
+++ Assets/_Scripts/MainMenuController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject exitPannel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonHandlerPlay()
+    {
+        SceneManager.LoadSceneAsync("LevelSelect");
+    }
+
+    public void ButtonHandlerLoad()
+    {
+
+    }
+
+    public void ButtonHandlerControls()
+    {
+
+    }
+
+    public void ButtonHandlerExit()
+    {
+        Debug.Log("Quiting Application");
+        Application.Quit();
+    }
+
+    public void ButtonHandlerExitPromptToggle()
+    {
+        exitPannel.SetActive(!exitPannel.activeSelf);
+    }
+}
